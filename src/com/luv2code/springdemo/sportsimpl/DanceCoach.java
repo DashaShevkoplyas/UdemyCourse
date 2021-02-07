f95a6d2e3ee94350d850f9dd1a205f3ccb17a115
DariaShevkoplias <shevkoplyasdarya23@gmail.com>
2021-02-07 21:32:46 +0200

    new object that implements Coach interface

---
+++ src/com/luv2code/springdemo/sportsimpl/DanceCoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo.sportsimpl;
+
+import com.luv2code.springdemo.interfaces.Coach;
+
+public class DanceCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Repeat new dance move fo 1h per day";
+	}
+
+}
